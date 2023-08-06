c85ec8e65b1d1e4a7f515ce71ec49b12ff49ae79
davidting0918 <dting0918@gmail.com>
2023-08-06 23:49:33 +0800

    Add JustLend grabber

--- grabber/JustLend.py
+++ grabber/JustLend.py
@@ -12,22 +12,22 @@ class JustLendClient(object):
     def get_currency_apy(self, currency=None):
         url = self.endpoint + "/lend/jtoken"
         res = requests.get(url).json()
-        data = res['data']['tokenList']
-        
+        data = res["data"]["tokenList"]
+
         # clean data
-        columns = ['symbol', 'borrowRate', 'supplyRate', 'totalBorrows', 'totalSupply']
+        columns = ["symbol", "borrowRate", "supplyRate", "totalBorrows", "totalSupply"]
         result = []
         if currency is None:
             for i in data:
-                i['symbol'] = i['symbol'][1:]
+                i["symbol"] = i["symbol"][1:]
                 result.append({column: i[column] for column in columns})
-                
+
             return result
-        
+
         else:
             currency = currency.upper()
             for i in data:
-                i['symbol'] = i['symbol'][1:]
-                if i['symbol'] == currency:
+                i["symbol"] = i["symbol"][1:]
+                if i["symbol"] == currency:
                     result.append({column: i[column] for column in columns})
                     return result
